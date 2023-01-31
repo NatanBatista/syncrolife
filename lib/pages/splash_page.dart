a3b2c0db4b4d565548098839023e9e2b2cb1bbbc
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-01-31 20:19:29 -0300

    Splash page criado

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Future.delayed(Duration(milliseconds: 3000))
+        .then((_) => Navigator.of(context).pushReplacementNamed('/singup'));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Color.fromARGB(255, 122, 135, 251),
+        child: Center(
+            child: Text(
+          "SyncroLife",
+          style: TextStyle(fontSize: 30, fontFamily: 'Montserrat'),
+        )),
+      ),
+    );
+    ;
+  }
+}
