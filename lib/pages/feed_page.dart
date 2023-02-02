5793b825d37a6ff620fd4b8d233fce8ab5bbb3d6
Lucas Menezes <lucas.menezesc120@gmail.com>
2023-02-01 21:14:53 -0300

    Template para mostrar médicos "Populares" entre outras categorias

--- lib/pages/feed_page.dart
+++ lib/pages/feed_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
+import '../styles.dart';
+
 class FeedPage extends StatefulWidget {
   const FeedPage({super.key});
 
@@ -31,7 +33,7 @@ class _FeedPageState extends State<FeedPage> {
                   child: Container(
                     decoration: BoxDecoration(
                         borderRadius: BorderRadius.circular(60),
-                        color: Color(0xFF0B2027)),
+                        color: charlestonGreen),
                     width: 40,
                     height: 40,
                     child: Icon(
@@ -40,18 +42,91 @@ class _FeedPageState extends State<FeedPage> {
                     ),
                   ),
                 ),
-                Text('Populares'),
-                ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 20,
-                    itemBuilder: (context, index) => Container(
-                          width: 130,
-                          height: 180,
-                          decoration: BoxDecoration(color: Colors.blue),
-                        ))
               ],
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 40, top: 50, bottom: 15),
+            child: Row(
+              children: [
+                Text(
+                  'Populares',
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+                ),
+                Expanded(child: Container())
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 180,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 20,
+              itemBuilder: (context, index) => Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Container(
+                  width: 130,
+                  height: 180,
+                  decoration: BoxDecoration(
+                    color: Colors.grey,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: Stack(alignment: Alignment.center, children: [
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+//Imagem do médico
+                      child: Image.asset(
+                        'assets/images/medico.png',
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: Container(
+                        width: 130,
+                        height: 180,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                            begin: Alignment.topCenter,
+                            end: Alignment.bottomCenter,
+                            colors: [
+                              Colors.transparent,
+                              Colors.black54,
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      top: 8,
+                      right: 8,
+                      child: Container(
+                        width: 25,
+                        height: 25,
+                        decoration: BoxDecoration(
+                            color: cornflowerBlue,
+                            borderRadius: BorderRadius.circular(30)),
+                        alignment: Alignment.center,
+//Avaliação do médico
+                        child: Text(
+                          '4.9',
+                          style: TextStyle(fontSize: 10, color: lavenderBlush),
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 15,
+                      child: Text('Fábio Augusto',
+                          style: TextStyle(
+                              fontSize: 14,
+                              color: lavenderBlush,
+                              fontWeight: FontWeight.w500)),
+                    )
+                  ]),
+                ),
+              ),
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/styles.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+//PALETA DE CORES
+Color charlestonGreen = Color(0xFF0B2027);
+Color greenSheen = Color(0xFF70A9A1);
+Color cornflowerBlue = Color(0xFF7A87FB);
+Color lavenderBlush = Color(0xFFF0E6EF);
+Color lightRed = Color(0xFFEF6461);
+
+//FONTES PREDEFINIDAS
+
+
