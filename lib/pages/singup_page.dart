fcc1b9b768eaf3df7b6662acfe5418113059d540
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-02-05 19:05:01 -0300

    Adicionando Scroll a página

--- lib/pages/singup_page.dart
+++ lib/pages/singup_page.dart
@@ -8,82 +8,82 @@ class SingupPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            ShaderMask(
-              shaderCallback: (bounds) => LinearGradient(
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                colors: [
-                  Color.fromARGB(255, 255, 255, 255),
-                  Color.fromARGB(0, 255, 255, 255)
-                ],
-                tileMode: TileMode.clamp,
-              ).createShader(bounds),
-              child: Image.asset("assets/images/medico1.png"),
-            ),
-            Container(
-                padding: EdgeInsets.all(55),
-                child: Column(
-                  children: [
-                    Text(
-                      "Registrar-se como:",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 65,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      child: ElevatedButton(
-                          style: ButtonStyle(
-                            backgroundColor:
-                                MaterialStateProperty.all(Colors.red),
-                          ),
-                          onPressed: () {},
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image(
-                                image: AssetImage('assets/icons/medico1.png'),
-                                width: 50,
-                                height: 50,
-                              ),
-                              Text("Médico")
-                            ],
-                          )),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-                    Container(
-                      height: 65,
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(30.0)),
-                      child: ElevatedButton(
-                          onPressed: () {},
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image(
-                                image:
-                                    AssetImage('assets/icons/medicacao1.png'),
-                                width: 50,
-                                height: 50,
-                              ),
-                              Text("Paciente")
-                            ],
-                          )),
-                    )
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              ShaderMask(
+                shaderCallback: (bounds) => LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color.fromARGB(255, 255, 255, 255),
+                    Color.fromARGB(0, 255, 255, 255)
                   ],
-                ))
-          ],
+                  tileMode: TileMode.clamp,
+                ).createShader(bounds),
+                child: Image.asset("assets/images/medico1.png"),
+              ),
+              Container(
+                  padding: EdgeInsets.all(55),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Registrar-se como:",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 65,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        child: ElevatedButton(
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all(Colors.red),
+                            ),
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image(
+                                  image: AssetImage('assets/icons/medico1.png'),
+                                  width: 50,
+                                  height: 50,
+                                ),
+                                Text("Médico")
+                              ],
+                            )),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Container(
+                        height: 65,
+                        child: ElevatedButton(
+                            onPressed: () {},
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Image(
+                                  image:
+                                      AssetImage('assets/icons/medicacao1.png'),
+                                  width: 50,
+                                  height: 50,
+                                ),
+                                Text("Paciente")
+                              ],
+                            )),
+                      )
+                    ],
+                  ))
+            ],
+          ),
         ),
       ),
     );
