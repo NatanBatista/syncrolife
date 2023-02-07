268db5720bed6640d6188b9833648877c92faa51
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-02-07 12:25:47 -0300

    Atualização dos TextFields e ElevatedButton
    Adicionando arrendondamento e backgroundColor aos TextFields e alterando a cor do ElevatedButton

--- lib/pages/signup1_page.dart
+++ lib/pages/signup1_page.dart
@@ -9,8 +9,70 @@ class SignUp1Page extends StatelessWidget {
         body: Center(
       child: Container(
         padding: EdgeInsets.all(55),
-        child: TextField(
-          decoration: const InputDecoration(label: Text("Nome")),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const TextField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Color(0xFFF0E6EF),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.transparent),
+                    borderRadius: BorderRadius.all(Radius.circular(20)), 
+                    ),
+                    label: Text("Nome"),
+                  ),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            const TextField(
+              decoration: InputDecoration(filled: true,
+                  fillColor: Color(0xFFF0E6EF),
+                  label: Text("Sobrenome"),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 0, 0, 0), width: 1))),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            TextField(
+              decoration: const InputDecoration(filled: true,
+                  fillColor: Color(0xFFF0E6EF),
+                  label: Text("CPF"),
+                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide(
+                          color: Color.fromARGB(255, 0, 0, 0), width: 1))),
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            Container(
+              // Primero botão
+              height: 65,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30.0),
+              ),
+              child: ElevatedButton(
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(Color(0xFF7A87FB)),
+                ),
+                onPressed: () {},
+                child: Row(
+                  // Linha que contém o icone e o nome do botão
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Próximo"), // Nome
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Icon(Icons.navigate_next)
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     ));
