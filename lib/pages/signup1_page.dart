48e42d4843fd1fa46776ac1976385baf6e68d4d8
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-07 22:33:07 -0300

    Adicionando widgets CustomTextField e CustomButtonIcon

--- lib/pages/signup1_page.dart
+++ lib/pages/signup1_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:syncrolife/widgets/cTextFieldForm.dart';
+
+import '../widgets/cElevatedButtonIcon.dart';
 
 class SignUp1Page extends StatelessWidget {
   const SignUp1Page({super.key});
@@ -12,65 +15,27 @@ class SignUp1Page extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const TextField(
-              decoration: InputDecoration(
-                  filled: true,
-                  fillColor: Color(0xFFF0E6EF),
-                  border: OutlineInputBorder(
-                    borderSide: BorderSide(color: Colors.transparent),
-                    borderRadius: BorderRadius.all(Radius.circular(20)), 
-                    ),
-                    label: Text("Nome"),
-                  ),
-            ),
+            const CustomTextField(label: 'Nome', fillColor: Color(0xFFF0E6EF)),
             SizedBox(
               height: 15,
             ),
-            const TextField(
-              decoration: InputDecoration(filled: true,
-                  fillColor: Color(0xFFF0E6EF),
-                  label: Text("Sobrenome"),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)),
-                      borderSide: BorderSide(
-                          color: Color.fromARGB(255, 0, 0, 0), width: 1))),
-            ),
+            const CustomTextField(label: 'Nome', fillColor: Color(0xFFF0E6EF)),
             SizedBox(
               height: 15,
             ),
-            TextField(
-              decoration: const InputDecoration(filled: true,
-                  fillColor: Color(0xFFF0E6EF),
-                  label: Text("CPF"),
-                  border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20)),
-                      borderSide: BorderSide(
-                          color: Color.fromARGB(255, 0, 0, 0), width: 1))),
-            ),
+            const CustomTextField(label: 'CPF', fillColor: Color(0xFFF0E6EF)),
             SizedBox(
               height: 15,
             ),
-            Container(
-              // Primero botão
-              height: 65,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30.0),
-              ),
-              child: ElevatedButton(
-                style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Color(0xFF7A87FB)),
-                ),
-                onPressed: () {},
-                child: Row(
-                  // Linha que contém o icone e o nome do botão
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text("Próximo"), // Nome
-                    SizedBox(
-                      height: 10,
-                    ),
-                    Icon(Icons.navigate_next)
-                  ],
-                ),
-              ),
+            CustomButtonIcon(
+              // ElevatedButton customizado
+              buttonText: 'Próximo', // Texto do botão
+              buttonColor: Color(0xFF7A87FB), // Background Color
+              textColor: Colors.white,
+              onPressed: () {
+                Navigator.pushNamed(context, '/signup');
+              },
+              icone: Icon(Icons.navigate_next),
             ),
           ],
         ),
