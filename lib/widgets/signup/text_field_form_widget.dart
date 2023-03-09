8e5e83515d2058fcfdad06af6873bb0d289f9ca3
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-08 21:34:09 -0300

    Widget campo de texto criado.
    Widget campo de texto customizado com cor e borda.

---
+++ lib/widgets/signup/text_field_form_widget.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  final String label;
+  final Color fillColor;
+  final TextEditingController? controller;
+  final bool obsText;
+
+  const CustomTextField({
+    Key? key,
+    required this.label,
+    required this.fillColor,
+    this.controller,
+    this.obsText = false,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      obscureText: obsText,
+      decoration: InputDecoration(
+        filled: true,
+        fillColor: fillColor,
+        border: const OutlineInputBorder(
+          borderSide: BorderSide(color: Colors.transparent),
+          borderRadius: BorderRadius.all(Radius.circular(25.0)),
+        ),
+        label: Text(label),
+      ),
+    );
+  }
+}
