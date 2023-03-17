7a2f9993d6ca9dfc5540570c8cfe2dfacffd8ddd
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-16 23:14:06 -0300

    Movendo widget

--- lib/pages/signup/widgets/signup_form_widget.dart
+++ lib/pages/signup/widgets/signup_form_widget.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:syncrolife/style.dart';
 import 'package:syncrolife/widgets/elevated_button_icon_widget.dart';
-import 'package:syncrolife/widgets/signup/text_field_form_widget.dart';
+import 'package:syncrolife/widgets/text_field_form_widget.dart';
 
 class CustomFormPage extends StatelessWidget {
   final String label1; //Label do campo 1
