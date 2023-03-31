99c3c75b2e3f953ff6cf07f40f263295871937e6
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-31 15:38:45 -0300

    Removendo import desnecessário e adicionando "const"

--- lib/styles.dart
+++ lib/styles.dart
@@ -1,18 +1,16 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 //PALETA DE CORES
-Color charlestonGreen = Color(0xFF0B2027);
-Color greenSheen = Color(0xFF70A9A1);
-Color cornflowerBlue = Color(0xFF7A87FB);
-Color lavenderBlush = Color(0xFFF0E6EF);
-Color lightRed = Color(0xFFEF6461);
+const Color charlestonGreen = Color(0xFF0B2027);
+const Color greenSheen = Color(0xFF70A9A1);
+const Color cornflowerBlue = Color(0xFF7A87FB);
+const Color lavenderBlush = Color(0xFFF0E6EF);
+const Color lightRed = Color(0xFFEF6461);
 
 //FONTES PREDEFINIDAS
 Text subTitulos(String texto) {
   return Text(
     texto,
-    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
+    style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w700),
   );
 }
