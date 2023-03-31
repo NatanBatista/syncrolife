8b68a888497333942f176fed3bf0f79cd52718a5
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-03-31 11:42:51 -0300

    Create recovery_page.dart

---
+++ lib/pages/recovery_page/recovery_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:syncrolife/styles.dart';
+import 'package:syncrolife/widgets/elevated_button_icon_widget.dart';
+import 'package:syncrolife/widgets/text_field_form_widget.dart';
+
+class RecoveryPage extends StatelessWidget {
+  const RecoveryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent, //Cor da AppBar
+        leading: IconButton(
+          //Botão com o icone
+          icon: const Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+          onPressed: () => (Navigator.pop(
+              context)), //Função Navigator para voltar para pagina anterior
+        ),
+      ),
+      body: Center(
+          child: Container(
+              padding: const EdgeInsets.all(55),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CustomTextField(label: "Email", fillColor: lavenderBlush),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CustomButtonIcon(
+                    // ElevatedButton customizado
+                    buttonText: "Proximo", // Texto do botão
+                    buttonColor: cornflowerBlue, // Background Color
+                    textColor: lavenderBlush,
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/recoverycode');
+                    },
+                    icone: const Icon(Icons.navigate_next),
+                  ),
+                ],
+              ))),
+    );
+  }
+}
