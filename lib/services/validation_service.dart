1bb29ca13ec289fa22d8a559f17b5490d248f2c7
Natan Batista <71360760+NatanBatista@users.noreply.github.com>
2023-04-12 13:51:16 -0300

    Atualizando e adicionando funções de formatação
    Atualizando interpolação de String a função de formatação do CPF.
    
    Função privada _getNumberOnly criada
    
    Função telefoneFormatter criada para formatar número na mascara (##) #####-####

--- lib/services/validation_service.dart
+++ lib/services/validation_service.dart
@@ -1,11 +1,14 @@
 class ValidationService {
+  static String _getNumbersOnly(String value) {
+    return value.replaceAll(RegExp(r'[^0-9]'), '');
+  }
+
   static bool isValidCPF(String cpf) {
     if (cpf == null || cpf.isEmpty) {
       return false;
     }
 
-    cpf =
-        cpf.replaceAll(RegExp(r'[^\d]'), ''); // remove caracteres não numéricos
+    cpf = _getNumbersOnly(cpf); // remove caracteres não numéricos
     if (cpf.length != 11) {
       return false;
     }
@@ -54,5 +57,42 @@ class ValidationService {
   static bool isValidEmail(String email) {
     String regex = r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$';
     return RegExp(regex).hasMatch(email);
+  }
+
+  static String cpfFormatter(String value) {
+    if (value.isEmpty) {
+      return '';
+    }
+
+    String cpf = _getNumbersOnly(value);
+
+    if (cpf.length > 11) {
+      cpf = cpf.substring(0, 11);
+    }
+
+    if (cpf.length < 4) {
+      return cpf;
+    } else if (cpf.length < 7) {
+      return '${cpf.substring(0, 3)}.${cpf.substring(3)}';
+    } else if (cpf.length < 10) {
+      return '${cpf.substring(0, 3)}.${cpf.substring(3, 6)}.${cpf.substring(6)}';
+    } else {
+      return '${cpf.substring(0, 3)}.${cpf.substring(3, 6)}.${cpf.substring(6, 9)}-${cpf.substring(9)}';
+    }
+  }
+
+  static String telefoneFormatter(String value) {
+    String numbers = _getNumbersOnly(value);
+    if (numbers.length > 11) {
+      numbers = numbers.substring(0, 11);
+    }
+
+    if (numbers.length < 3) {
+      return numbers;
+    } else if (numbers.length < 8) {
+      return '(${numbers.substring(0, 2)}) ${numbers.substring(2)}';
+    } else {
+      return '(${numbers.substring(0, 2)}) ${numbers.substring(2, 7)}-${numbers.substring(7)}';
+    }
   }
 }
